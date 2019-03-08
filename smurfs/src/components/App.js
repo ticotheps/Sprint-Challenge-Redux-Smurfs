37c8b6e9e85b0ceba9b6287fd2e91989865e0e3a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 12:41:07 -0500

    Created a new component <SmurfsList /> and added it to the rendered <div> in App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -11,13 +12,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfsList />
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapStateToProps = state => ({
+  smurfs: [],
+  fetchingSmurfs: state.fetchingSmurfs,
+  addingSmurf: state.addingSmurf,
+  updatingSmurf: state.updatingSmurf,
+  deletingSmurf: state.deletingSmurf,
+  error: null
+});
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(App);
