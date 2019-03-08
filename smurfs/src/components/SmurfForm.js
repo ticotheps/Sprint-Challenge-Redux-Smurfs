2e29fe13478f54ef21e01ef02353782c6479dc09
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 17:07:20 -0500

    Converted <SmurfForm /> functional component into a class component

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const SmurfForm = () => { 
-    return (
-        <div className="smurf-form-container">
-            <form>
-                <input type="text"/>
-            </form>
-        </div>
-    );        
+class SmurfForm extends React.Component {
+
+    render() {
+        return (
+            <div className="smurf-form-container">
+                <h2 className="smurf-form-heading">Add a Smurf the the Village!</h2>
+                <form className="smurf-form">
+                    <input type="text"/>
+                </form>
+            </div>
+        ); 
+    }
 }
 
 export default SmurfForm;
