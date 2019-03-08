7003c70893fcb453b9919ff6f3868460286bbb5a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 13:07:23 -0500

    Converted <App /> component into a functional component, imported BrowserRouter into App.js and created Route for <SmurfsList /> component

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getSmurfs } from './actions';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -8,14 +7,15 @@ import './App.css';
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
-class App extends Component {
-  render() {
-    return (
+function App () {
+  return (
+    <Router>
       <div className="App">
-        <SmurfsList />
+        <Route path="/smurfs" component={SmurfsList} />
       </div>
-    );
-  }
+    </Router>
+
+  );    
 }
 
 const mapStateToProps = state => ({
