0a1cd58974e3523bb40b8dbf5a8847311ba6a7f0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 16:30:57 -0500

    Added 'id' property into 'smurfs' variable in server.js file

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -4,7 +4,6 @@ const Smurf = (props) => {
     return (
         <div className="smurf-card-container">
            <h2>{props.name}</h2>
-           <img src={props.imageUrl} alt={props.name} />
            <h3>{props.age}</h3>
            <h3>{props.height}</h3>
         </div>
