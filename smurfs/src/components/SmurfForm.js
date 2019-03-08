6d190c5fe7cf26e7bed2ec79515a9dcebaffdfab
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 17:20:24 -0500

    Created <input> elements for each smurf property inside of <form>

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -13,14 +13,35 @@ class SmurfForm extends React.Component {
     render() {
         return (
             <div className="smurf-form-container">
-                <h2 className="smurf-form-heading">Add a Smurf the the Village!</h2>
+                <h2 className="smurf-form-heading">Add a NEW Smurf the the Village!</h2>
                 <form className="smurf-form">
                     <input 
                         type="text"
                         onChange={this.handleInputChange}
-                        placeholder="New Smurf's Name"
+                        placeholder="Name"
                         value={this.state.name}
                         name="name"
+                    />
+                    <input 
+                        type="text"
+                        onChange={this.handleInputChange}
+                        placeholder="Age"
+                        value={this.state.age}
+                        name="age"
+                    />
+                    <input 
+                        type="text"
+                        onChange={this.handleInputChange}
+                        placeholder="Height"
+                        value={this.state.height}
+                        name="height"
+                    />
+                    <input 
+                        type="text"
+                        onChange={this.handleInputChange}
+                        placeholder="Image URL"
+                        value={this.state.image}
+                        name="image"
                     />
                 </form>
             </div>
