8ed4025a57ca577ebd02c2c1b77344c6b02d61e7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 17:15:30 -0500

    Styled form

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class SmurfForm extends React.Component {
+    state = {
+        smurf: {
+            name: '',
+            age: '',
+            height: '',
+            image: ''
+        }
+    }
 
     render() {
         return (
             <div className="smurf-form-container">
                 <h2 className="smurf-form-heading">Add a Smurf the the Village!</h2>
                 <form className="smurf-form">
-                    <input type="text"/>
+                    <input 
+                        type="text"
+                        onChange={this.handleInputChange}
+                        placeholder="New Smurf's Name"
+                        value={this.state.name}
+                        name="name"
+                    />
                 </form>
             </div>
         ); 
