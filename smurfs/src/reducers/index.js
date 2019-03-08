df3afba655ba2a402205764b6ad2630f3611af62
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 12:06:47 -0500

    Imported 'rootReducer' into index.js(src), passed in 'rootReducer' to createStore() function, and passed in 'thunk' and 'logger' as arguments inside of 'applyMiddleware()' function

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,18 +2,22 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
 
+const initialState = {
+   smurfs: [],
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: null
+};
+
+function reducer(state=intialState, action) {
+  console.log("The reducer function is firing! Yay!", action);
+  return state;
+}
+
+export default reducer;
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
